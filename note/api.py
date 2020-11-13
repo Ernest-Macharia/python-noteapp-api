35c1986010a1d8a637b2769275f1e46efc01bce0
Ernest-Macharia <ernesthuku@gmail.com>
2020-11-13 14:31:23 +0300

    changes

--- note/api.py
+++ note/api.py
@@ -2,4 +2,4 @@ from rest_framework import routers
 from noteapp import views as myapp_views
 
 router = routers.DefaultRouter()
-router.register(myapp_views.NoteViewSet)
+router.register(r'notes', myapp_views.NoteViewSet)
