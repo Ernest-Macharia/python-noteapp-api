8db897c0fd1d3c7a01f5520cdb7fc6bff41f754e
Ernest-Macharia <ernesthuku@gmail.com>
2020-11-13 14:31:49 +0300

    changes

--- note/urls.py
+++ note/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 from .api import router
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/notes', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
