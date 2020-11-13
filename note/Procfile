a35e7550d74ba9d5c86012e8fb7b249adad0a484
Ernest-Macharia <ernesthuku@gmail.com>
2020-11-13 14:15:49 +0300

    changes

---
+++ note/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn note.wsgi:application --log-file -
--- note/settings.py
+++ note/settings.py
@@ -121,5 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 django_heroku.settings(locals())
